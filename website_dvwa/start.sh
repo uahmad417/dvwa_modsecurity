6ee2b02f8dada1b687c0e0f306c2b795e4c42ac2
Orochinagi <18u.ahmad@gmail.com>
2022-07-31 18:19:19 +0500

    Website created Created Dockerfile for php website DVWA. Also added a startup script start.sh to configure dvwa using environment variables and to keep the container running

---
+++ website_dvwa/start.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sed -i "s/[ [\']db_server[\'] ]   = [\']127.0.0.1[\']/\'db_server\' ]   = \'$DB_SERVER\'/g" dvwa/config/config.inc.php
+sed -i "s/[ [\']db_database[\'] ] = [\']dvwa[\']/\'db_database\' ] = \'$DB_DATABASE\'/g" dvwa/config/config.inc.php
+sed -i "s/[ [\']db_user[\'] ]     = [\']dvwa[\']/\'db_user\' ]     = \'$DB_USER\'/g" dvwa/config/config.inc.php
+sed -i "s/[ [\']db_password[\'] ] = [\']p@ssw0rd[\']/\'db_password\' ] = \'$DB_PASSWORD\'/g" dvwa/config/config.inc.php
+sed -i "s/[ [\']db_port[\']] = [\']3306[\']/\'db_port\'] = \'$DB_PORT\'/g" dvwa/config/config.inc.php
+sed -i "s/[ [\']default_security_level[\'] ] = \'impossible\'/\'default_security_level\' ] = \'$DVWA_SEC_LEVEL\'/g" dvwa/config/config.inc.php
+service apache2 start
+tail -f /var/log/apache2/error.log
